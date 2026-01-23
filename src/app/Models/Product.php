02e1fb983b835475605eedfbfd6f593fa27886c3
wako nomura <nom.wako369@gmail.com>
2026-01-23 22:49:39 +0900

    ProductsテーブルとConditionsテーブルの作成

---
+++ src/app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'condition_id',
+        'name',
+        'brand',
+        'description',
+        'price',
+        'image_path',
+        'is_sold'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function condition()
+    {
+        return $this->belongsTo(Condition::class);
+    }
+}
---
+++ src/database/migrations/2026_01_23_215905_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('condition_id')->constrained()->restrictOnDelete();
+            $table->string('name');
+            $table->string('brand')->nullable();
+            $table->text('description');
+            $table->unsignedInteger('price');
+            $table->string('image_path');
+            $table->boolean('is_sold')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
