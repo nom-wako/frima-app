ae77b7a541d828bd190d9bcebe446fee56d97a7c
wako nomura <nom.wako369@gmail.com>
2026-02-23 21:11:26 +0900

    商品一覧画面調整

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::all();
-        return view('products.index', compact('products'));
+        $tab = $request->query('tab');
+
+        if ($tab === 'mylist') {
+            $user = Auth::user();
+            $products = ($user && method_exists($user, 'facoriteProducts')) ? $user->favoriteProducts : collect();
+        } else {
+            $products = Product::all();
+        }
+        $products = $products ?? collect();
+        return view('products.index', compact('products', 'tab'));
     }
 
     public function show(Product $product)
--- src/resources/views/products/index.blade.php
+++ src/resources/views/products/index.blade.php
@@ -5,16 +5,22 @@
 @endsection
 
 @section('content')
-<div class="product__content">
-  <ul class="product-list">
-    @foreach ($products as $product)
-    <li class="product-list__item">
-      <a href="{{ route('products.show', $product) }}" class="product-list__link">
-        <div class="product-list__img"><img src="{{ asset('storage/' . $product->image_path) }}" alt=""></div>
-        <p class="product-list__name">{{ $product->name }}</p>
-      </a>
-    </li>
-    @endforeach
-  </ul>
+<div class="tabs">
+  <a href="/" class="tab-item {{ request('tab') !== 'mylist' ? 'is-active' : '' }}">おすすめ</a>
+  <a href="/?tab=mylist" class="tab-item {{ request('tab') === 'mylist' ? 'is-active' : '' }}">マイリスト</a>
 </div>
+@if($products->isEmpty())
+<p class="product-list__empty">お気に入り登録した商品はまだありません。</p>
+@else
+<ul class="product-list">
+  @foreach ($products as $product)
+  <li class="product-list__item">
+    <a href="{{ route('products.show', ['product' => $product->id]) }}" class="product-list__link">
+      <div class="product-list__img"><img src="{{ asset('storage/' . $product->image_path) }}" alt=""></div>
+      <p class="product-list__name">{{ $product->name }}</p>
+    </a>
+  </li>
+  @endforeach
+</ul>
+@endif
 @endsection
